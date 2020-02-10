b4750baf32a02c9c11fa27c4bb3ca79def36d157
ghanizadev <ghanizadev@gmail.com>
2020-02-10 03:11:15 -0300

    mocha timeout increased from 5s to 10s

--- .mocharc.js
+++ .mocharc.js
@@ -7,7 +7,7 @@ module.exports = {
   package: './package.json',
   reporter: 'spec',
   slow: 75,
-  timeout: 5000,
+  timeout: 10000,
   ui: 'bdd',
   'watch-ignore': ['test/utils'],
   exit: true,
